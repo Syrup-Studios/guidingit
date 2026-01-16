397c9ffc8faa9a8adced3b1797681a49c0985fa0
SodaSyrup <salad@bysalad.studio>
2026-01-16 08:31:21 +0100

    crack or something

--- shell.nix
+++ shell.nix
@@ -13,9 +13,41 @@ let
 
     propagatedBuildInputs = with pkgs.python313Packages; [
       beautifulsoup4
-      mkdocs
       packaging
-      pymdown-extensions
+    ];
+
+    doCheck = false;
+  };
+    mkdocs-video = pkgs.python313Packages.buildPythonPackage rec {
+    pname = "mkdocs-video";
+    version = "1.5.0";
+    format = "wheel";
+
+    src = pkgs.fetchurl {
+      url = "https://files.pythonhosted.org/packages/cc/3a/b51e305eca21cdd58f4eb06973099ba0ea679b1f437a92a49f8fc576310e/mkdocs_video-1.5.0-py3-none-any.whl";
+      sha256 = "b35613d4dacbac2dfa94d8c2600383cda14ad99a1fa1542b5fc4e9c6d19e9fe1";
+    };
+
+    propagatedBuildInputs = with pkgs.python313Packages; [
+      beautifulsoup4
+      packaging
+    ];
+
+    doCheck = false;
+  };
+      mkdocs-open-in-new-tab = pkgs.python313Packages.buildPythonPackage rec {
+    pname = "mkdocs-open-in-new-tab";
+    version = "1.0.8";
+    format = "wheel";
+
+    src = pkgs.fetchurl {
+      url = "https://files.pythonhosted.org/packages/21/94/44f3c868495481c868d08eea065c82803f1affd8553d3383b782f497613c/mkdocs_open_in_new_tab-1.0.8-py3-none-any.whl";
+      sha256 = "051d767a4467b12d89827e1fea0ec660b05b027c726317fe4fceee5456e36ad2";
+    };
+
+    propagatedBuildInputs = with pkgs.python313Packages; [
+      beautifulsoup4
+      packaging
     ];
 
     doCheck = false;
@@ -28,12 +60,19 @@ pkgs.mkShell {
     (pkgs.python313.withPackages (ps: with ps; [
       mkdocs
       mkdocs-material
-      mkdocs-awesome-nav
       mkdocs-git-authors-plugin
       mkdocs-git-revision-date-localized-plugin
+      mkdocs-git-committers-plugin-2
+      mkdocs-markmap
+      mkdocs-glightbox
+      mkdocs-awesome-nav
+      mkdocstrings
+      mkdocs-autorefs
+      mkdocs-meta-descriptions-plugin
       mkdocs-minify-plugin
       pymdown-extensions
-      mkdocs-meta-descriptions-plugin
+      mkdocs-video
+      mkdocs-open-in-new-tab
     ]))
   ];
 }
